7b0985c149ad6f4bf8f672604f81c38b5779fc11
vandalise <95403169+vandalise@users.noreply.github.com>
2021-12-18 16:48:01 -0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+import requests, time, random, string
+from colorama import Fore, Back, Style
+
+def randomstring():
+    x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for i in range(5))
+    return x
+
+def main(tokens, channelid, name):
+   while True:
+       for token in tokens:
+            try:
+                threadname = f"{name} {randomstring()}"
+                r = requests.post(f"https://discord.com/api/v{random.randint(6,9)}/channels/{channelid}/threads", headers={"content-type": "application/json", "Authorization": token}, json={"name": threadname, "type": 11, "auto_archive_duration": 60})
+                if r.status_code == 200 or r.status_code == 201:
+                    print(f"{Fore.GREEN}[+] Created thread : {threadname}")
+                elif r.status_code == 429:
+                    timex = int(r.json()["retry_after"])
+                    print(f"{Fore.RED}[-] Ratelimited | Retrying after {timex} seconds...")
+                    time.sleep(10) # i did this instead of sleeping for timex cuz I wanted to see the ratelimit time get updated
+            except Exception as e:
+                print(f"{Fore.RED}[-] {e}")
+
+print(Fore.YELLOW)
+tokens = []
+amount_of_tokens = int(input("Amount of tokens: "))
+for i in range(amount_of_tokens):
+    tokens.append(input(f"Token: "))
+channelid = input("Channel ID: ")
+name = input("Thread Name: ")
+main(tokens=tokens, channelid=channelid, name=name)
